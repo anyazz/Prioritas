481789170d8595769ed5f0a55845558f21982b5d
Anya Zhang <Anya@Anyas-MacBook.local>
2016-12-27 20:12:31 -0700

    3rd try

--- application.py
+++ application.py
@@ -281,7 +281,7 @@ def getTime():
     category=request.args.get("category"))
     return jsonify(time)
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
