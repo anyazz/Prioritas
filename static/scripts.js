12613630074e429598fce9d9b2d570423cb927d9
Anya Zhang <Anya@Anyas-MacBook.local>
2016-12-31 19:16:41 -0700

    v3.01

--- static/scripts.js
+++ static/scripts.js
@@ -2,10 +2,10 @@
 const ENTER_KEY = 13;
 const FLASH_MS = 400;
 const CHECK_MS = 100;
-const TIMER_SECS =11;
-const BREAK_SECS=6;
+const TIMER_SECS =1501;
+const BREAK_SECS=301;
 const SECS_PER_MIN = 60;
-const DONE_MS = 1000;
+const DONE_MS = 900;
 const GREEN = "#518e37";
 const BLACK = "#000000";
 const RED = "#ad544e";
